2f2a1e2bd42b178564eed3c8bce8d7fb7e979355
Sithis562 <65447162+Sithis562@users.noreply.github.com>
2022-03-13 13:06:11 +0300

    Add files via upload

---
+++ Library/Account.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Library
+{
+    public interface IListOfAccounts
+    {
+        public List<Account<long, int>> ListOfAccounts(string path)
+        {
+            List<Account<long, int>> accounts = new List<Account<long, int>>();
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] arg = sr.ReadLine().Split(' ');
+
+                    accounts.Add(new Account<long, int>(Convert.ToInt32(arg[0]), Convert.ToInt64(arg[1]), Convert.ToInt32(arg[2]), arg[3], arg[4]));
+                }
+            }
+
+            return accounts;
+        }
+    }
+
+    public interface IAddAccount : IListOfAccounts
+    {
+        public void AddAccount(Account<long, int> account, string path)
+        {
+            List<Account<long, int>> accounts = new List<Account<long, int>>();
+
+            accounts.Add(account);
+
+            string[] accountstxt = new string[accounts.Count];
+
+            for (int i = 0; i < accounts.Count; i++)
+            {
+                accountstxt[i] = accounts[i].ToString();
+            }
+
+            File.AppendAllLines(path, accountstxt);
+
+        }
+    }
+
+    public interface ITransfer : IListOfAccounts
+    {
+        public void Transfer(string from, string to, string Amount, string path)
+        {
+            string accountstxt = "accounts.txt";
+
+            List<Account<long, int>> accounts = new List<Account<long, int>>(ListOfAccounts(accountstxt));
+
+            for (int i = 0; i < accounts.Count; i++)
+            {
+                if (from.Contains(Convert.ToString(accounts[i].Number)))
+                {
+                    for (int j = 0; j < accounts.Count; j++)
+                    {
+                        if (to.Contains(Convert.ToString(accounts[j].Number)))
+                        {
+                            int amount = Convert.ToInt32(Amount);
+
+                            accounts[j].Amount = accounts[j].Amount + amount;
+                            accounts[i].Amount = accounts[i].Amount - amount;
+                        }
+                    }
+                }
+            }
+            string[] accountsList = new string[accounts.Count];
+
+            for (int i = 0; i < accounts.Count; i++)
+            {
+                accountsList[i] = accounts[i].ToString();
+            }
+
+            File.WriteAllLines(accountstxt, accountsList);
+        }
+    }
+
+    public class Account<T1, T2> : IAddAccount, ITransfer
+    {
+        protected int id;
+        protected T1 number;
+        protected T2 amount;
+        protected string status;
+        protected string type;
+
+        public int ID { get { return this.id; } }
+
+        /// <summary>
+        /// Номер счёта
+        /// </summary>
+        public T1 Number { get { return this.number; } }
+
+        /// <summary>
+        /// Средства
+        /// </summary>
+        public T2 Amount { get { return this.amount; } set { this.amount = value; } }
+
+        /// <summary>
+        /// Статус, открыт/закрыт
+        /// </summary>
+        public string Status { get { return this.status; } set { this.status = value; } }
+
+        /// <summary>
+        /// Тип, депозитный/недепозитный
+        /// </summary>
+        public string Type { get { return this.type; } }
+
+        public Account(int ID, T1 Number, T2 Amount, string Status, string Type)
+        {
+            id = ID;
+            number = Number;
+            amount = Amount;
+            status = Status;
+            type = Type;
+        }
+
+        public Account() : this(0, default, default, "", "")
+        {
+        }
+
+        public override string ToString()
+        {
+            return $"{ID} {Number} {Amount} {Status} {Type}";
+        }
+    }
+}
---
+++ Library/Client.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Library
+{
+    public interface IListOfClients
+    {
+        public List<Client> ListOfClients(string path)
+        {
+            var clients = new List<Client>();
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] arg = sr.ReadLine().Split(' ');
+
+                    clients.Add(new Client(int.Parse(arg[0]), arg[1], arg[2], arg[3]));
+                }
+            }
+
+            return clients;
+        }
+    }
+
+    public class Client : Account<long, int>, IListOfClients
+    {
+        protected int idc;
+        protected string firstName;
+        protected string lastName;
+        protected string patronymic;
+
+        public int IDc { get { return this.idc; } }
+
+        /// <summary>
+        /// Имя
+        /// </summary>
+        public string FirstName { get { return this.firstName; } }
+
+        /// <summary>
+        /// Фамилия
+        /// </summary>
+        public string LastName { get { return this.lastName; } }
+
+        /// <summary>
+        /// Отчество
+        /// </summary>
+        public string Patronymic { get { return this.patronymic; } }
+
+        /// <summary>
+        /// Конструктор клиента
+        /// </summary>
+        public Client(int IDc, string LastName, string FirstName, string Patronymic)
+        {
+            idc = IDc;
+            lastName = LastName;
+            firstName = FirstName;
+            patronymic = Patronymic;
+        }
+
+        public Client() : this(0, "", "", "")
+        {
+        }
+
+        public override string ToString()
+        {
+            return $"{IDc} {LastName} {FirstName} {Patronymic}";
+        }
+    }
+}
